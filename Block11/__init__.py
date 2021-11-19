b18fcaf221bdd700aac9c05f4c0cf90c0ed8d0e8
Leonard Reidy <leonard.reidy@gmail.com>
2021-11-19 12:59:10 +0000

    Correct inaccurate comment in init

--- Block11/__init__.py
+++ Block11/__init__.py
@@ -2,6 +2,6 @@ import Live
 from .Block11 import Block11 
 
 def create_instance(c_instance):
-    ' Creates and returns the Block11 script '
+    ' Creates and returns the Block11 instance '
     return Block11(c_instance)
 
