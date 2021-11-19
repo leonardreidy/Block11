5ee3624aa71a8ccf6380466f0bb3fa2ee678c269
Leonard Reidy <leonard.reidy@gmail.com>
2021-11-19 12:26:33 +0000

    Fix bug in LividBlock0 init

--- LividBlock0/__init__.py
+++ LividBlock0/__init__.py
@@ -6,7 +6,7 @@ from .Block import Block
 from _Framework.Capabilities import controller_id, inport, outport, CONTROLLER_ID_KEY, PORTS_KEY, HIDDEN, NOTES_CC, SCRIPT, REMOTE, SYNC, TYPE_KEY, FIRMWARE_KEY, AUTO_LOAD_KEY
 
 def get_capabilities():
-    return {CONTROLLER_ID_KEY: controller_id(vendor_id="04D8", product_ids=["fd33"], model_name="Livid Block"),
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[115], model_name="Livid Block"),
         PORTS_KEY: [inport(props=[HIDDEN, NOTES_CC, SCRIPT, REMOTE]),
                     inport(props = []),
                     outport(props=[HIDDEN, NOTES_CC, SCRIPT, REMOTE]),
