1b13a361dd3f99c383e57fe9156b92459d85cd44
Leonard Reidy <leonard.reidy@gmail.com>
2021-11-20 22:33:33 +0000

    Map encoder K8 to crossfader control

--- Block11/MIDI_Map.py
+++ Block11/MIDI_Map.py
@@ -240,7 +240,7 @@ BUTTON_VECTOR_CH = (0, #Global play 				[0] Channel
 
 SLIDER_VECTOR = (8, #Master track volume			[0]
                  9, #Cue level control				[1]
-                -1, #Crossfader control				[2]
+                 7, #Crossfader control				[2]
                 -1, #Tempo control					[3]
                 )
 
